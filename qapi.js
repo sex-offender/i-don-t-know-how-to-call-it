e36320ee38d927570bcf006aa86d6bd68a0c7c90
sex-offender <82478311+sex-offender@users.noreply.github.com>
2021-04-14 01:32:12 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60761712bae31a001e3fe49a",
+				"60761712bae31a001e3fe49c"
+			]
+		},
+		"60761712bae31a001e3fe49a": {
+			"id": "60761712bae31a001e3fe49a",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60761712bae31a001e3fe49c": {
+			"id": "60761712bae31a001e3fe49c",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
